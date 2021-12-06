3193a312c49524900eb9570b42936bc3b125f7ed
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2021-12-06 20:04:15 -0300

    port root files to TS

--- src/sideloadUtils.ts
+++ src/sideloadUtils.ts
@@ -8,25 +8,27 @@ const require = createRequire(import.meta.url);
 (function () {
     let childProcess = require('child_process');
     let oldSpawn = childProcess.spawn;
-    function newSpawn() {
-        if (process.env.LOGLEVEL >= 3) console.info('spawn called:', arguments);
+    function newSpawn(this: any) {
+        if (Number(process.env.LOGLEVEL) >= 3) console.info('spawn called:', arguments);
         return oldSpawn.apply(this, arguments);
     }
     childProcess.spawn = newSpawn;
 })();
 
-/* Regex String Escaper */
+
+/* Regex String Escaper */ //@ts-ignore // TODO: Make this TypeScript compliant
 RegExp.quote = (str) => {
     return str.replace(/([.?*+^$[\]\\(){}|-])/g, '\\$1');
 }
 
-/* Regex Discord Markdown Escapers */
+/* Regex Discord Markdown Escapers */ //@ts-ignore // TODO: Make this TypeScript compliant
 RegExp.escapeMarkdown = (text) => {
     text = String(text);
     if (!text) return null;
     return text.replace(/\\/g, '\\\\').replace(/_/g, '\\_').replace(/\*/g, '\\*').replace(/\[/g, '\\[').replace(/\]/g, '\\]')
                .replace(/\(/g, '\\(').replace(/\)/g, '\\)').replace(/</g, '\\<').replace(/>/g, '\\>').replace(/`/g, '\\`');
 }
+//@ts-ignore
 RegExp.escapeBacktick = (text) => {
     return text.replace(/`/g, '​`'); // Prepends ZWS
 }
@@ -34,7 +36,7 @@ RegExp.escapeBacktick = (text) => {
 /* IANA TLD list fetcher  */
 got.get('https://data.iana.org/TLD/tlds-alpha-by-domain.txt').then(response => {
     const listData = response.body.split('\n');
-    const listVersion = listData.shift().slice(2);
+    const listVersion = listData.shift()!.slice(2);
     listData.pop();
-    fs.writeFileSync(join(process.env.workdir, './bot/assets/tlds.json'), JSON.stringify({ version: listVersion, tlds: listData }));
+    fs.writeFileSync(join(process.env.workdir!, './bot/assets/tlds.json'), JSON.stringify({ version: listVersion, tlds: listData }));
 });
--- src/webserver.ts
+++ src/webserver.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import { Request } from 'express-serve-static-core';
+import { ParsedQs } from 'qs';
 const app = express();
 
-app.get('/', (request, response) => {
+app.get('/', (request, response): void => {
     if (process.env.LOG_WEBSERVER) logRequest(request);
     response.status(200).send('Webserver is online.');
 });
@@ -9,8 +11,7 @@ app.get('/', (request, response) => {
 app.listen(process.env.PORT);
 if (process.env.LOG_WEBSERVER) console.info(`[WEBSERVER] Listening on PORT ${process.env.PORT}`);
 
-function logRequest(req) {
+function logRequest(req: Request<{}, any, any, ParsedQs, Record<string, any>>): void {
     const date = new Date().toISOString();
-    const logstr = `[WEBSERVER-${date}] ${req.method} > ${req.hostname}${req.url}`;
-    console.log(logstr);
+    return console.log(`[WEBSERVER-${date}] ${req.method} > ${req.hostname}${req.url}`);
 };
