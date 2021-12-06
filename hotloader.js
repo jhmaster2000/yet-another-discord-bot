c94c86b5c0fa4ef8aaaba3ef4c7fb86c034dcbec
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2021-12-06 19:34:22 -0300

    update hotloader.js

--- hotloader.js
+++ hotloader.js
@@ -1,18 +1,9 @@
 import { URL } from 'url'
 
 export const resolve = async (specifier, context, defaultResolve) => {
-    const result = defaultResolve(specifier, context, defaultResolve)
-    const child = new URL(result.url)
+    const result = defaultResolve(specifier, context, defaultResolve);
+    const child = new URL(result.url);
 
-    if (
-        child.protocol === 'nodejs:' ||
-        child.protocol === 'node:' ||
-        child.pathname.includes('/node_modules/')
-    ) {
-        return result
-    }
-
-    return {
-        url: child.href + '?id=' + Math.random().toString(36).substring(3),
-    }
+    if (child.protocol === 'nodejs:' || child.protocol === 'node:' || child.pathname.includes('/node_modules/')) return result;
+    else return { url: child.href + '?id=' + Math.random().toString(36).substring(3) };
 }
