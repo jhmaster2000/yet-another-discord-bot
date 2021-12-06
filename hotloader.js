28fdcd64c2cab072933c39a8dda4f1c54a01b19b
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2021-12-06 19:54:20 -0300

    initial typescript preparations

--- hotloader.js
+++
@@ -1,9 +0,0 @@
-import { URL } from 'url'
-
-export const resolve = async (specifier, context, defaultResolve) => {
-    const result = defaultResolve(specifier, context, defaultResolve);
-    const child = new URL(result.url);
-
-    if (child.protocol === 'nodejs:' || child.protocol === 'node:' || child.pathname.includes('/node_modules/')) return result;
-    else return { url: child.href + '?id=' + Math.random().toString(36).substring(3) };
-}
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 process.on('uncaughtException', err => console.error('<!!!> UNCAUGHT ERROR:', err));
-process.env.VERSION_TIMESTAMP = new Date().valueOf();
-if (fs.existsSync('./.git/refs/heads/main')) process.env.GIT_HASH = fs.readFileSync('./.git/refs/heads/master').toString().trim(); // comment out this line if not using git
+process.env.VERSION_TIMESTAMP = new Date().valueOf().toString();
+if (fs.existsSync('./.git/refs/heads')) process.env.GIT_HASH = fs.readFileSync('./.git/refs/heads/master').toString().trim(); // comment out this line if not using git
 process.env.workdir = dirname(fileURLToPath(import.meta.url));
 
 console.info(
