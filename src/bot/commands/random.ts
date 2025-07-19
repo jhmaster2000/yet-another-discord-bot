eed48bd4668def1f865ed957660aa7dacb1f45c4
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2025-07-19 04:18:21 -0300

    rng cmd upd

--- src/bot/commands/random.ts
+++ src/bot/commands/random.ts
@@ -4,59 +4,60 @@ import fs from 'fs';
 import { join } from 'path';
 import Bot from '../Bot.js';
 import { type Args } from '../events/messageCreate.js';
+
 const assets = JSON.parse(fs.readFileSync(join(process.env.workdir!, './bot/assets/random.json')).toString()) as RandomAssets;
 const items = ['cat', 'dog', 'bench'] as const;
+type Items = typeof items[number];
+type ItemsClass = { [key in Items]: (message: Message) => void; };
+let xmark: string;
 
 interface RandomAssets {
     readonly benches: string[];
-    dogs: string[];
-    cats: string[];
 }
 
-await got.get('https://random.dog/doggos').then(response => {
-    assets.dogs = (<string[]>JSON.parse(response.body)).filter((dog: string) => !dog.endsWith('.mp4'));
-});
+interface CatDogApiResponse {
+    id: string,
+    url: string,
+    width: number,
+    height: number,
+}
 
-export function run(client: Bot, message: Message, argsx: Args) {
-    if (!argsx.basic.length) return invalidArguments(client.em.xmark, message);
+export async function run(client: Bot, message: Message, argsx: Args) {
+    xmark ??= client.em.xmark;
+    if (!argsx.basic.length) return invalidArguments(message);
     const args = argsx.basic.map(arg => arg.raw);
-    if (items.includes(<'cat' | 'dog' | 'bench'>args[0].toLowerCase())) return random[<keyof typeof random>args[0]](message);
-    else return invalidArguments(client.em.xmark, message);
+    if (items.includes(<Items>args[0].toLowerCase())) return Random[args[0] as Items](message);
+    else return invalidArguments(message);
 }
-
-function invalidArguments(xmark: string, message: Message) {
+function invalidArguments(message: Message) {
     return message.channel.send(`${xmark} You need to tell me what to get a random of!\nValid options: \`${items.join('`, `')}\``);
 }
 
-const random = {
-    cat: (message: Message) => {
-        void got.get('http://aws.random.cat/meow').then(response => {
-            const body = JSON.parse(response.body) as { file: string };
-            const catEmbed = new Discord.EmbedBuilder()
-                .setTitle('Here\'s your random cat! 🐱')
-                .setImage(body.file);
-            return void message.channel.send({ embeds: [catEmbed] });
-        });
-    },
-    dog: (message: Message) => {
-        const selected = assets.dogs[Math.floor(Math.random() * assets.dogs.length)];
-        const dogEmbed = new Discord.EmbedBuilder()
-            .setTitle('Here\'s your random dog! 🐶')
-            .setImage(`https://random.dog/${selected}`);
-        return message.channel.send({ embeds: [dogEmbed] });
-    },
-    bench: (message: Message) => {
+const Random = (class {
+    static async #catOrDog(message: Message, type: 'cat' | 'dog') {
+        const response = await got.get(`https://api.the${type}api.com/v1/images/search?limit=1`)
+            .catch(() => void message.channel.send(`${xmark} Failed to get a random ${type}!`));
+        if (!response) return;
+        const body = JSON.parse(response.body) as CatDogApiResponse[];
+        const embed = new Discord.EmbedBuilder()
+            .setTitle(`Here's your random ${type}! ${type === 'cat' ? '🐱' : '🐶'}`)
+            .setImage(body[0].url);
+        return message.channel.send({ embeds: [embed] });
+    }
+    static cat = (message: Message) => void Random.#catOrDog(message, 'cat');
+    static dog = (message: Message) => void Random.#catOrDog(message, 'dog');
+    static bench(message: Message) {
         const selected = assets.benches[Math.floor(Math.random() * assets.benches.length)];
-        const benchEmbed = new Discord.EmbedBuilder()
+        const embed = new Discord.EmbedBuilder()
             .setTitle('Here\'s your random bench! 🪑')
             .setImage(selected);
-        return message.channel.send({ embeds: [benchEmbed] });
+        return void message.channel.send({ embeds: [embed] });
     }
-}
+}) satisfies ItemsClass;
 
 export const config = {
-    aliases: ['rand'],
-    selfperms: ['EMBED_LINKS'],
+    aliases: ['rand', 'rng'],
+    selfperms: ['EmbedLinks'] satisfies Discord.PermissionsString[],
     description: 'Generic command to get a random image of something.',
     usage: {
         args: '<cat/dog/bench>'
