af15becda3f11ead1f73024f204154eb8c756239
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2025-07-19 04:13:44 -0300

    ddg command update

--- src/bot/commands/ddg.ts
+++ src/bot/commands/ddg.ts
@@ -11,21 +11,51 @@ const enum Types {
     exclusive = 'E'
 }
 
+type DDGNull = '';
+type HTMLString = string;
+type Result = {
+    FirstURL: string;
+    Icon: {
+        Height: number | DDGNull;
+        URL: string;
+        Width: number | DDGNull;
+    };
+    Result: HTMLString;
+    Text: string;
+};
+type InfoboxField = {
+    data_type: string;
+    label: string;
+    value: unknown;
+};
+type Infobox = {
+    content: (InfoboxField & { wiki_order: number | string })[];
+    meta: InfoboxField[];
+};
+
 interface DuckDuckGoResponse {
-    Type?: Types;
-    Redirect?: string;
-    AbstractURL?: string;
-    Heading: string;
+    Type: Types;
+    Abstract: string;
     AbstractSource: string;
-    AbstractText?: string;
-    Image?: string;
-    RelatedTopics: {
-        index: number;
-        topic: string;
-        FirstURL: string;
-    }[];
-    Answer?: string;
-    AnswerType?: string;
+    AbstractText: string;
+    AbstractURL: string;
+    Answer: string;
+    AnswerType: string;
+    Definition: string;
+    DefinitionSource: string;
+    DefinitionURL: string;
+    Entity: string;
+    Heading: string;
+    Image: string;
+    ImageHeight: number | DDGNull;
+    ImageIsLogo: number | DDGNull;
+    ImageWidth: number | DDGNull;
+    Infobox: Infobox | DDGNull;
+    OfficialDomain?: string;
+    OfficialWebsite?: string;
+    Redirect: string;
+    RelatedTopics: Result[];
+    Results: Result[];
 }
 
 export async function run(client: Bot, message: Message, argsx: Args) {
@@ -35,7 +65,10 @@ export async function run(client: Bot, message: Message, argsx: Args) {
     const query = args.join(' ').toLowerCase();
     return got.get(`https://api.duckduckgo.com/?q=${encodeURIComponent(query)}&format=json&no_html=1&skip_disambig=1&no_redirect=1`).then(response => {
         const data = JSON.parse(response.body) as DuckDuckGoResponse;
-        if (!data.Type || data.Redirect) return void message.channel.send(`${client.em.xmark} No results found!`);
+        if (!data.Type || data.Redirect) {
+            console.log('[DDG-BADRES]', query, data);
+            return void message.channel.send(`${client.em.xmark} No valid results found!*`);
+        }
 
         if (data.AbstractURL) {
             if (data.Type === Types.disambig) data.Heading = data.Heading + ' (disambiguation)';
@@ -46,7 +79,7 @@ export async function run(client: Bot, message: Message, argsx: Args) {
                 .setURL(data.AbstractURL)
                 .setFooter({ text: `Result type: web` })
                 .setColor('Orange');
-            let ellipsis = (data.AbstractText!.length > 200) ? '...' : '';
+            let ellipsis = (data.AbstractText.length > 200) ? '...' : '';
             if (data.AbstractText) ddgEmbed.setDescription(data.AbstractText.slice(0, 200).trim() + ellipsis);
             if (data.Image) ddgEmbed.setThumbnail('https://api.duckduckgo.com' + data.Image);
 
@@ -63,7 +96,7 @@ export async function run(client: Bot, message: Message, argsx: Args) {
             }
             return void message.channel.send({ embeds: [ddgEmbed] });
         } else if (data.Answer) {
-            if (data.AnswerType === 'ip' || data.AnswerType === 'iploc') return void message.channel.send(`${client.em.xmark} No results found!`);
+            if (data.AnswerType === 'ip' || data.AnswerType === 'iploc') return void message.channel.send(`${client.em.xmark} No valid results found!`);
             if (data.AnswerType === 'color_code') {
                 const colorHex = data.Answer.slice(6).split(' ~ ')[0].trim();
                 const ddgEmbed = new Discord.EmbedBuilder()
@@ -78,11 +111,13 @@ export async function run(client: Bot, message: Message, argsx: Args) {
             const ddgEmbed = new Discord.EmbedBuilder()
                 .setAuthor({ name: 'DuckDuckGo QuickSearch Results:', iconURL: 'https://duckduckgo.com/assets/icons/meta/DDG-icon_256x256.png' })
                 .setDescription(`${data.Answer}`)
-                .setFooter({ text: `Result type: ${data.AnswerType!.replace(/_/g, ' ') || '-'}` })
+                .setFooter({ text: `Result type: ${data.AnswerType.replace(/_/g, ' ') || '-'}` })
                 .setColor('DarkAqua');
             return void message.channel.send({ embeds: [ddgEmbed] });
+        } else {
+            console.log('[DDG-NORES]', query, data, data);
+            return void message.channel.send(`${client.em.xmark} No results found!`);
         }
-        return;
     }).catch(err => {
         console.error(err);
         void message.channel.send(`${client.em.xmark} An error occured when trying to search. Try again later.`);
@@ -91,7 +126,7 @@ export async function run(client: Bot, message: Message, argsx: Args) {
 
 export const config = {
     aliases: ['ducksearch', 'duckduckgo', 'ddgsearch', 'quicksearch'],
-    selfperms: ['EMBED_LINKS'],
+    selfperms: ['EmbedLinks'] satisfies Discord.PermissionsString[],
     description: 'Searches DuckDuckGo for information.',
     usage: {
         args: '<...search>'
